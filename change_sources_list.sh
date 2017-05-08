9a1f46967bcaf2ea8df7752f09af43646541d174
wayneg123 <2211403035@smtp.ujs.edu.cn>
2017-05-08 16:58:16 +0800

    fix sources.list's address

--- change_sources_list.sh
+++ change_sources_list.sh
@@ -1,5 +1,6 @@
 #/bin/bash
+sudo apt-get install -y curl
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak && \
-wget https://raw.githubusercontent.com/wayneg123/zshrc/master/sources.list && \
+wget https://raw.githubusercontent.com/wayneg123/linux_configuration/master/sources.list && \
 sudo cp sources.list /etc/apt/sources.list
 sudo apt-get update
