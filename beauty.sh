9648b810aa8c96e2ab6e00a0bd331de746a70ab7
wayneg123 <2211403035@stmail.ujs.edu.cn>
2017-05-08 21:22:26 +0800

    add beauty.sh

---
+++ beauty.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y unity-tweak-tool && \
+sudo add-apt-repository -y ppa:noobslab/themes && \
+sudo apt-get update && \
+sudo apt-get install -y flatabulous-theme && \
+sudo add-apt-repository -y ppa:noobslab/icons && \
+sudo apt-get update && \
+sudo apt-get install ultra-flat-icons
