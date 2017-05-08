6cb9dda326ac9da7010db5dc58b8a3904d68759a
wayneg123 <2211403035@stmail.ujs.edu.cn>
2017-05-08 20:49:57 +0800

    add ros_install.sh

---
+++ ros_install.sh
@@ -0,0 +1,4 @@
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full -y
