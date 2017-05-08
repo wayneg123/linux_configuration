6cd7d65e363e22e1c19c65906dc789ad6080a85d
CharlesGao <2211403035@stmail.ujs.edu.cn>
2017-05-08 23:35:08 +0800

    change sources.list to ustc

--- change_sources_list.sh
+++ change_sources_list.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt-get install -y curl
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak && \
-wget https://raw.githubusercontent.com/wayneg123/linux_configuration/master/sources.list && \
-sudo cp sources.list /etc/apt/sources.list
+wget https://raw.githubusercontent.com/wayneg123/linux_configuration/master/sources_ustc.list && \
+sudo cp sources_ustc.list /etc/apt/sources.list
 sudo apt-get update
