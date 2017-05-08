d253087b16182323dfa59dd2ff95c04a59f09f3e
wayneg123 <2211403035@smtp.ujs.edu.cn>
2017-05-08 15:26:38 +0800

    add sudo before oh-my-zsh install

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get install screen git python-pip autojump zsh -y
 sudo pip install powerline-status
 sudo chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
