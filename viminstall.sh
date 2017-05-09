39c12665209bf8126bbe554159cde52b5bd354f8
wayneg123 <2211403035@stmail.ujs.edu.cn>
2017-05-09 18:44:56 +0800

    fix make install and delete python3 interp

--- viminstall.sh
+++ viminstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-
+pip install jedi
 sudo apt-get remove --purge -y vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common
 sudo apt-get install -y liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev mercurial libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev
 
@@ -9,8 +9,9 @@ sudo mkdir -p /usr/include/lua5.1/include
 sudo ln -s /usr/include/luajit-2.0 /usr/include/lua5.1/include
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd ~
 git clone https://github.com/vim/vim.git
-cd vim
+cd ~/vim
 make distclean
 
 ./configure --with-features=huge \
@@ -20,8 +21,6 @@ make distclean
             --enable-netbeans \
             --enable-pythoninterp \
             --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-python3interp \
-            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
             --enable-luainterp \
             --with-luajit \
             --with-lua-prefix=/usr/include/lua5.1 \
@@ -30,9 +29,8 @@ make distclean
             --enable-cscope \
             --prefix=/usr 2>config_err.log >config.log || cat config_err.log
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
-apt-get install -y checkinstall
 cd ~/vim
-make install
+sudo make install
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
 sudo update-alternatives --set editor /usr/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
